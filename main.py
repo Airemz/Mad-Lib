3f100194a9bcacbbd16ae36edac91d80d5a9d557
Sadiq R <113649841+Airemz@users.noreply.github.com>
2023-05-09 18:33:27 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+print("Enter some funny words to make it funny")
+
+buildings=input("Enter a plural noun: ")
+Manhattan=input("Enter a location: ")
+completed=input("Enter a past-tense verb: ")
+destroyed=input("Enter another past-tense verb: ")
+Larry_Silverstein=input("Enter a name: ")
+
+print(""""
+7 World Trade Center (7 WTC or WTC-7) refers to two %s that have existed at the same location within the World Trade Center site in Lower %s, New York City. The original structure, part of the original World Trade Center, was %s in 1987 and was %s in the September 11 attacks in 2001. The current structure opened in May 2006. Both buildings were developed by %s, who holds a ground lease for the site from the Port Authority of New York and New Jersey.
+"""
+% (buildings, Manhattan, completed, destroyed, Larry_Silverstein))
